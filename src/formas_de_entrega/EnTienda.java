243d3271ba228508cd5031748deca654daaf87fe
colpim2 <pamela.castle@hotmail.com>
2021-02-03 18:59:03 -0600

    Recibo.txt creado

--- src/formas_de_entrega/EnTienda.java
+++ src/formas_de_entrega/EnTienda.java
@@ -1,6 +1,8 @@
 package formas_de_entrega;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
@@ -17,7 +19,7 @@ public class EnTienda {
 
     /** Método para Generar los Archivos y Directorios Necesarios */
     private int GenerarArchivos(){
-        String pathRecibo = "./recibos/Recibo"+ contador +".txt";
+        String pathRecibo = "./recibos/Recibo_"+ contador +".txt";
 
         File directorioRecibo= new File("./recibos");
         File archivoRecibo = new File(pathRecibo);
@@ -54,12 +56,23 @@ public class EnTienda {
     /** Método GuardarRecibo */
     private void GuardarRecibo(String nombre,List<String> productos,List<Float> precios,float CantidadAPagar){
         String pathRecibo = "./recibos/Recibo_"+ contador +".txt";
-        File recibo= new File(pathRecibo);
+        File archivoRecibo= new File(pathRecibo);
+        try{
+            FileWriter archivoOut = new FileWriter(archivoRecibo, true);
 
-        System.out.println("Atendido por: "+nombre+"\nProductos comprados:");
-        for (int i = 0; i < productos.size(); i++)
-            System.out.println("  "+productos.get(i)+" -------> "+precios.get(i));
-        System.out.println("Total: "+CantidadAPagar);
+            String texto = "Atendido por: "+nombre+"\nProductos comprados:";
+            for (int i = 0; i < productos.size(); i++)
+                texto += "\n  " +productos.get(i)+" -------> "+precios.get(i);
+            texto += "\nTotal: "+CantidadAPagar;
+
+            char buffer[] = new char[texto.length()];
+            texto.getChars(0, texto.length(), buffer, 0);
+            archivoOut.write(buffer);
+            archivoOut.close();
+        }
+        catch (IOException ioException){
+            System.out.println("ERROR: No se pudo abrir el archivo");
+        }
     }
 
     /** Método Imprimir Recibo */
